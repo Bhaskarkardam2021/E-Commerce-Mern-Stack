2577fd7b4b2a2409f8e7280dae0c11a0975b63c5
BHASKAR KARDAM <84039829+Bhaskarkardam2021@users.noreply.github.com>
2024-01-18 00:23:15 +0530

    Project Installation

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 4000
+
+app.use("/",(req,res)=>{
+    res.send("hello from server")
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT} Port`)
+})
